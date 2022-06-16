5f4b7f5842686c197b35ef18732cbcdb54d7c465
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-06-15 23:28:10 -0600

    feat: create login route

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,5 +1,36 @@
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const { compareSync } = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const uuid = require('uuid/v4');
+const userService = require('../services/user');
+const refreshTokenService = require('../services/refreshToken');
+const config = require('../config');
 
 const router = new Router();
+
+async function issueTokenPair(userId) {
+  const newRefreshToken = uuid();
+  await refreshTokenService.add({
+    token: newRefreshToken,
+    userId,
+  });
+
+  return {
+    token: jwt.sign({ id: userId }, config.secret),
+    refreshToken: newRefreshToken,
+  };
+}
+
+router.post('/login', bodyParser(), async ctx => {
+  const { login, password } = ctx.request.body;
+  const user = await userService.find({ login });
+  if (!user || !compareSync(password, user.password)) {
+    const error = new Error();
+    error.status = 403;
+    throw error;
+  }
+  ctx.body = await issueTokenPair(user.id);
+});
 
 module.exports = router;
