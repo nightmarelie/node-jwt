157fbfab090453afab758ce29272e2f85d36b823
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-06-16 23:03:11 -0600

    feat: move helper in sep dir

---
+++ tests/auth.js
@@ -0,0 +1,22 @@
+const { test } = require('ava');
+const agent = require('supertest-koa-agent');
+const createApp = require('../src/app');
+
+const app = agent(createApp());
+
+test('User can succesfully login', async t => {
+  const res = await app.post('/auth/login').send({
+    login: 'user',
+    password: 'user',
+  });
+  t.is(res.status, 200);
+  t.truthy(typeof res.body.token === 'string');
+  t.truthy(typeof res.body.refreshToken === 'string');
+
+  const refreshTokenRes = await app.post('/auth/refresh').send({
+    refreshToken: res.body.refreshToken,
+  });
+  t.is(refreshTokenRes.status, 200);
+  t.truthy(typeof refreshTokenRes.body.token === 'string');
+  t.truthy(typeof refreshTokenRes.body.refreshToken === 'string');
+});
