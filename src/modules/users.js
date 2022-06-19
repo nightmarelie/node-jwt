9fd402aed1ae89520e99feaca32ba0325229acd8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-06-18 23:40:33 -0600

    feat: add user controller for fetching user by id

--- src/modules/users.js
+++ src/modules/users.js
@@ -1,11 +1,20 @@
 const Router = require('koa-router');
-const { list } = require('../services/user');
+const { list, find } = require('../services/user');
 
 const router = new Router();
 
 router.get('/', async ctx => {
   const data = await list();
   ctx.body = data;
+});
+
+router.get('/:id', async ctx => {
+  const user = await find({
+    id: Number(ctx.params.id),
+  });
+  if (user) {
+    ctx.body = user;
+  }
 });
 
 module.exports = router;
