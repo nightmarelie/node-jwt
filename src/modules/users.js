fa64c5ffde99d3d2bb6f5d07d7881f834e907058
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-06-18 23:37:24 -0600

    feat: add user list controller

--- src/modules/users.js
+++ src/modules/users.js
@@ -1,5 +1,11 @@
 const Router = require('koa-router');
+const { list } = require('../services/user');
 
 const router = new Router();
+
+router.get('/', async ctx => {
+  const data = await list();
+  ctx.body = data;
+});
 
 module.exports = router;
